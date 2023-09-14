9a01ff251f40e3e8b83f5c927df1bb22c89179a5
lidor koresh <lidor123@gmail.com>
2023-09-14 10:20:31 +0200

    newfeature3

--- system-specs.ps1
+++ system-specs.ps1
@@ -3,11 +3,12 @@ $computerInfo = Get-CimInstance -ClassName CIM_OperatingSystem
 $totalMemory = $computerInfo.TotalVisibleMemorySize / 1MB
 $freeMemory = $computerInfo.FreePhysicalMemory / 1MB
 $usedMemory = $totalMemory - $freeMemory
-$memoryUsagePercentage = [math]::Round((($usedMemory / $totalMemory) * 100), 2)
-$publicIP = (Invoke-RestMethod -uri "http://ipinfo.io/ip").ToString()
+$memoryUsagePercentage = [math]::Round(($usedMemory / $totalMemory) * 100, 2)
+$publicIP = (Invoke-RestMethod -Uri "http://ipinfo.io/ip").ToString()
+
 # Get CPU Information
 $cpuInfo = Get-CimInstance -ClassName Win32_Processor
-$cpuUsagePercentage = [math]::Round(($cpuInfo.LoadPercentage), 2)
+$cpuUsagePercentage = [math]::Round($cpuInfo.LoadPercentage, 2)
 
 # Get Disk Information for each drive
 $diskInfos = Get-CimInstance -ClassName Win32_LogicalDisk
@@ -19,16 +20,16 @@ $hostName = [System.Net.Dns]::GetHostName()
 $ipAddress = [System.Net.Dns]::GetHostAddresses($hostName) | Where-Object { $_.AddressFamily -eq 'InterNetwork' } | Select-Object -ExpandProperty IPAddressToString
 
 # Get Total Memory in bytes
-$totalMemoryBytes = (Get-CimInstance -ClassName CIM_OperatingSystem).TotalVisibleMemorySize * 1024
+$totalMemoryBytes = ($computerInfo.TotalVisibleMemorySize * 1024)
 
 # Get Total CPU Time
 $totalCPUTime = (Get-Process | Measure-Object -Property CPU -Sum).Sum
 
 # Get Top 5 Processes by CPU
-$topCPUProcesses = Get-Process | Sort-Object -Property CPU -Descending | Select-Object -First 5 -Property Name, @{Name="CPU(%)";Expression={"{0:N2}" -f (($_.CPU / $totalCPUTime) * 100)}}
+$topCPUProcesses = Get-Process | Sort-Object -Property CPU -Descending | Select-Object -First 5 -Property Name, @{Name="CPU(%)"; Expression={"{0:N2}" -f (($_.CPU / $totalCPUTime) * 100)}}
 
 # Get Top 5 Processes by Memory
-$topMemoryProcesses = Get-Process | Sort-Object -Property WS -Descending | Select-Object -First 5 -Property Name, @{Name="Memory(%)";Expression={"{0:N2}" -f (($_.WS / $totalMemoryBytes) * 100)}}
+$topMemoryProcesses = Get-Process | Sort-Object -Property WS -Descending | Select-Object -First 5 -Property Name, @{Name="Memory(%)"; Expression={"{0:N2}" -f (($_.WS / $totalMemoryBytes) * 100)}}
 
 # Create HTML content
 $htmlContent = @"
@@ -37,51 +38,44 @@ $htmlContent = @"
 <head>
     <title>System Information</title>
     <style>
+        /* New styles for the tables */
+        table {
+            border-collapse: collapse;
+            width: 50%;
+            margin: auto;
+            margin-top: 20px;
+            margin-bottom: 20px;
+        }
+        th, td {
+            border: 1px solid #ddd;
+            padding: 8px;
+            text-align: left;
+        }
+        th {
+            background-color: #0000FF;
+            color: white;
+        }
+        tr:nth-child(even) { /* Stripe every other row */
+            background-color: #f2f2f2;
+        }
 
-
-
-
-    /* New styles for the tables */
-    table {
-        border-collapse: collapse;
-        width: 50%;
-        margin: auto
-        margin-top: 20px;
-        margin-bottom: 20px;
-    }
-    th, td {
-        border: 1px solid #ddd;
-        padding: 8px;
-        text-align: left;
-    }
-    th {
-        background-color: #0000FF;
-        color: white;
-    }
-    tr:nth-child(even) { /* Stripe every other row */
-        background-color: #f2f2f2;
-    }
-
-
-
-#new table for adding logs and events
-
-table.myTable {
-    width: 100%;
-    border-collapse: collapse;
-}
-table.myTable th {
-    background-color: #0000FF;
-    color: white;
-}
-table.myTable th, table.myTable td {
-    padding: 15px;
-    text-align: left;
-    border-bottom: 1px solid #ddd;
-}
-table.myTable tr:hover {background-color: #f5f5f5;}
-
-
+        /* Additional styles */
+        table.myTable {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        table.myTable th {
+            background-color: #0000FF;
+            color: white;
+        }
+        table.myTable th, table.myTable td {
+            padding: 15px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
+        table.myTable tr:hover {
+            background-color: #f5f5f5;
+        }
 
         body { font-family: Arial, sans-serif; }
         h1 { color: #333; }
@@ -102,8 +96,8 @@ table.myTable tr:hover {background-color: #f5f5f5;}
 </head>
 <body>
     <h1>System Information</h1>
-    <h2>Host: $hostName , Ip address: $ipAddress</h2>
-    <h2>public ip:  $publicIP</h2>
+    <h2>Host: $hostName , IP Address: $ipAddress</h2>
+    <h2>Public IP: $publicIP</h2>
     <div class="info">
         <strong>Memory Usage:</strong> $memoryUsagePercentage%
         <div class="bar">
@@ -117,15 +111,14 @@ table.myTable tr:hover {background-color: #f5f5f5;}
         </div>
     </div>
 "@
-
+# Loop through disk information and add to HTML content
 foreach ($diskInfo in $diskInfos) {
     $driveLetter = $diskInfo.DeviceID
     $totalDiskSpace = $diskInfo.Size / 1GB
     $freeDiskSpace = $diskInfo.FreeSpace / 1GB
     $usedDiskSpace = $totalDiskSpace - $freeDiskSpace
-    $diskUsagePercentage = [math]::Round((($usedDiskSpace / $totalDiskSpace) * 100), 2)
+    $diskUsagePercentage = [math]::Round(($usedDiskSpace / $totalDiskSpace) * 100, 2)
 
-    # Add Disk Usage to HTML content
     $htmlContent += @"
     <div class="info">
         <strong>Disk Usage ($driveLetter):</strong> $diskUsagePercentage%
@@ -136,10 +129,9 @@ foreach ($diskInfo in $diskInfos) {
 "@
 }
 
-
 # Add Top CPU Processes to HTML content
 $htmlContent += "<h2>Top 5 Processes by CPU Usage:</h2>"
-$htmlContent += "<table><tr><th>Name</th><th>CPU Usage (%)</th></tr>"
+$htmlContent += "<table class='myTable'><tr><th>Name</th><th>CPU Usage (%)</th></tr>"
 foreach ($process in $topCPUProcesses) {
     $htmlContent += "<tr><td>$($process.Name)</td><td>$($process.'CPU(%)')</td></tr>"
 }
@@ -147,7 +139,7 @@ $htmlContent += "</table>"
 
 # Add Top Memory Processes to HTML content
 $htmlContent += "<h2>Top 5 Processes by Memory Usage:</h2>"
-$htmlContent += "<table><tr><th>Name</th><th>Memory Usage (%)</th></tr>"
+$htmlContent += "<table class='myTable'><tr><th>Name</th><th>Memory Usage (%)</th></tr>"
 foreach ($process in $topMemoryProcesses) {
     $htmlContent += "<tr><td>$($process.Name)</td><td>$($process.'Memory(%)')</td></tr>"
 }
@@ -158,13 +150,12 @@ $htmlContent += @"
 </html>
 "@
 
-# Get the most recent errors from the System log
+# Get the most recent errors from the System log and Application log
 $systemErrors = Get-WinEvent -LogName System -MaxEvents 10 | Where-Object { $_.LevelDisplayName -eq 'Error' }
-
-# Get the most recent errors from the Application log
 $appErrors = Get-WinEvent -LogName Application -MaxEvents 10 | Where-Object { $_.LevelDisplayName -eq 'Error' }
 
 # Combine the errors and select the properties to display
+
 $errors = $systemErrors + $appErrors | Select-Object TimeCreated, LevelDisplayName, LogName, Message
 
 # Convert the errors to an HTML table
@@ -178,10 +169,6 @@ $htmlContent += "<h2>Recent System and Application Errors:</h2>"
 $htmlContent += $errorTable
 
 # Your existing code...
-
-
-
-
 
 # Write HTML content to a file
 $htmlContent | Out-File -FilePath "info.html"
